f1d49d4508ec0c8c29786a16ab40cf01febe1536
simo-andreev <simeon.zlatanov.andreev@abv.bg>
2017-05-06 21:44:04 +0300

    Accout slightly remodeled

--- app/src/main/java/bg/o/sim/finansizmus/model/Account.java
+++ app/src/main/java/bg/o/sim/finansizmus/model/Account.java
@@ -3,127 +3,50 @@ package bg.o.sim.finansizmus.model;
 import android.support.annotation.NonNull;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
 
-/**
- */
-public class Account implements Serializable, Comparable<Account>, RowDisplayable {
+public class Account implements Serializable, RowDisplayable {
+
+    private long id;
+    private long userFk;
 
     private String name;
-    private long id;
     private int iconId;
-    private double sum;
-    private long userFk;
-    private HashMap<Category.Type,ArrayList<Transaction>> transactions;
 
-    /**
-     * Account constructor.
-     * @param name Account name : a non-null and not-empty String.
-     * @param iconId Resource ID for icon image.
-     */
-    public Account(@NonNull String name, int iconId) {
+    public Account(@NonNull String name, int iconId, long id, long userFk) {
+        //TODO - revisit validation
         if (name == null || name.isEmpty())
             throw new IllegalArgumentException("The Section name must be a non-null and not-empty string!");
         if (iconId == -1)
             throw new IllegalArgumentException();
 
-        this.sum = 0.0;
         this.name = name;
         this.iconId = iconId;
 
-        transactions = new HashMap<>();
-        transactions.put(Category.Type.EXPENSE, new ArrayList<Transaction>());
-        transactions.put(Category.Type.INCOME, new ArrayList<Transaction>());
-    }
-
-
-    public long getId() {
-        return id;
-    }
-
-    public long getUserFk() {
-        return userFk;
-    }
-
-    public void setUserFk(long userFk) {
+        this.id = id;
         this.userFk = userFk;
     }
 
     @Override
-    public int compareTo(@NonNull Account o) {
-        return this.name.compareTo(o.name);
+    public long getId() {
+        return id;
     }
-
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Account)) return false;
-
-        Account section = (Account) o;
-
-        return (name.equals(section.name));
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = name.hashCode();
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return this.name;
-    }
-
     public String getName() {
         return name;
     }
-
+    @Override
     public int getIconId() {
         return iconId;
     }
+    @Override
+    public void setName(String newName) {
+        if (name == null || name.isEmpty()) return;
+        this.name = newName;
+    }
 
     @Override
-    public boolean getIsFavourite() {
-        return false;
-    }
-
-    public double getSum(){
-        double sum = 0.0;
-        for ( Transaction t : transactions.get(Category.Type.INCOME))
-            sum += t.getSum();
-        for ( Transaction t : transactions.get(Category.Type.EXPENSE))
-            sum -= t.getSum();
-        return sum;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setIconId(int iconId) {
-        this.iconId = iconId;
-    }
-
-    public List<Transaction> getTransactions() {
-        ArrayList<Transaction> tempTransactions = new ArrayList<>();
-        tempTransactions.addAll(transactions.get(Category.Type.EXPENSE));
-        tempTransactions.addAll(transactions.get(Category.Type.INCOME));
-
-        return Collections.unmodifiableList(tempTransactions);
-    }
-
-    public boolean addTransaction(Transaction entry) {
-        if (entry == null || entry.getCategory() == null) return false;
-        Category.Type type = entry.getCategory().getType();
-        return transactions.get(type).add(entry);
+    public void setIconId(int newIconId) {
+        //TODO - how to validate this is an existing mipmap's id?
+        if (newIconId > 0) this.iconId = newIconId;
     }
 }
